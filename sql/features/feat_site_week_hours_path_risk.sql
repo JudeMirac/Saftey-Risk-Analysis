03c4ba7b634123e16a1862ef53e4d095ff2536de
JudeMirac <Judesonmirac@gmail.com>
2026-01-20 02:47:44 -0500

    Safety Project

--- sql/features/feat_site_week_hours_path_risk.sql
+++ sql/features/feat_site_week_hours_path_risk.sql
@@ -1,4 +1,7 @@
 -- feature build out for the process path x weekly exposure 
+-- KPI: Incidents + YTD metrics  
+-- Trend: Rolling weekly incidents + hours 
+-- signal: NWeekly incident rate/ hours + normalized rates  
 
 DROP VIEW IF EXISTS feat_site_week_hours_path_risk; 
 
@@ -8,65 +11,133 @@ with base AS (
     site, 
     fiscal_year, 
     week_sum, 
-    incidents, 
-    fclm_hours, 
-    process_path 
+    incidents as incidents_ytd, -- KPI (cumalative incidents)
+    fclm_hours, -- signal
+    process_path -- KPI
+
 FROM int_site_week_hours_path),
+
+base_weekly AS (
+select *,
+
+-- incidents metric is a running total/cumalative results, this is unefficient when it comes to analysis. 
+-- Convert cumalative incidents metric into a weekly delata 
+-- weekly delta = cumalative incidents current week - cumalative incidents privous week
+
+    (incidents_ytd - LAG(incidents_ytd, 1, 0) OVER 
+        (PARTITION by site, fiscal_year, process_path
+            ORDER by week_sum) 
+                )as incidents_weekly
+
+FROM base), 
+    
 
 rolling AS ( 
     select *,  
-        avg(fclm_hours) over            -- avg amount of hour per process path 
-        (PARTITION by process_path
-            ORDER BY fiscal_year, week_sum)
-                as avg_hours_per_path, 
+        avg(fclm_hours) over            -- Rolling Hourly averages over 4 weeks (Trend)
+            (PARTITION by site, fiscal_year, process_path
+                ORDER BY fiscal_year, week_sum
+                    ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
+                        as avg_hours_per_path,  
 
-        avg(incidents) over         
-            (PARTITION by process_path
-            ORDER BY fiscal_year, week_sum) 
-                as avg_incident_per_path, 
+ 
+        avg(incidents_weekly) over    -- Average incident over 4 weeks (TREND)
+            (PARTITION by site, fiscal_year, process_path
+                ORDER BY fiscal_year, week_sum
+                 ROWS BETWEEN 3 PRECEDING AND CURRENT ROW) 
+                    as avg_incident_per_path, 
 
     SQRT(
-        AVG(incidents * incidents) OVER -- standard deviation 4wk rolling 
+        AVG(fclm_hours * fclm_hours) OVER                -- rolling deviation over 4 weeks for fclm hours (Trend) 
             (PARTITION by process_path
-            ORDER BY fiscal_year, week_sum
-            ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+                ORDER by week_sum 
+                    ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+        )
+        -
+        (
+            AVG(fclm_hours) OVER 
+                (PARTITION BY process_path
+                    ORDER BY week_sum
+                        ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+        )
+        *
+            AVG(fclm_hours) OVER 
+                (PARTITION BY process_path
+                    ORDER BY week_sum 
+                        ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
+                            ) AS fclm_hours_4wk_std,
+
+
+    SQRT(
+        AVG(incidents_weekly * incidents_weekly) OVER -- Standard deviation over 4 week averages for incidents (TREND) 
+            (PARTITION by process_path
+                ORDER BY fiscal_year, week_sum
+                    ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
         ) 
         -
         ( 
-        AVG(incidents) OVER 
+        AVG(incidents_weekly) OVER 
             (PARTITION by process_path
-            ORDER BY fiscal_year, week_sum
-            ROWS BETWEEN 3 PRECEDING AND CURRENT ROW 
+                ORDER BY fiscal_year, week_sum
+                    ROWS BETWEEN 3 PRECEDING AND CURRENT ROW 
         )
         *
-        AVG(incidents) OVER 
+        AVG(incidents_weekly) OVER 
             (PARTITION by process_path
-            ORDER BY fiscal_year, week_sum
-            ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
-        )
-    )
-) AS incident_4wk_std
+                ORDER BY fiscal_year, week_sum
+                    ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+                )
+            )
+        ) AS incident_4wk_std
 
-FROM base), 
+FROM base_weekly), 
 
-ranked AS (                 
+rates AS (
+
     select *,                       
-        incidents * 10000.0/nullif(fclm_hours, 0)   --- incidents per 10000 hours 
-            as incidents_per_10k_hours, 
+    -- normalized rate  
+     --- signal Incidents per 1000 hours
+        incidents_weekly * 1000.0/nullif(fclm_hours, 0)   --- Incidents per 1000 hours 
+            as incidents_per_1k_hours, 
 
+    -- deviation for the norm 
+    -- Trend (This flags week where hours are unusual relative to history) 
+    (fclm_hours - avg_hours_per_path) as hours_deviation, -- signal (This flags week where hours are unusual relative to history) 
+
+    -- Percent deviation 
+    -- Trend (This gives a scaled percentage output on whether the week is above or under the normal in terms of hours 
+    (fclm_hours - avg_hours_per_path * 1.0/nullif(avg_hours_per_path, 0) as deviation_pct 
+
+    
+
+FROM rolling), 
+
+ranked AS (
+    SELECT *, 
+    -- Rank top path by desceding order
         RANk () OVER 
-            (PARTITION by site,           
-            fiscal_year, week_sum 
-            ORDER by incidents DESC)            -- rank top path by desceding order
-                as process_path_rank 
-FROM base)
+            (PARTITION by site, fiscal_year, week_sum 
+                ORDER by incidents_per_1k_hours DESC)            
+                    AS process_path_rank 
+FROM rates)
 
-SELECT *, -- display the top 3 process path for incidents  
-CASE                   
-    WHEN process_path_rank <= 3 THEN 1 ELSE 0 END AS top_3_flag_path        
+SELECT *,  
+
+CASE       -- display the top 3 process path for incidents  
+    WHEN process_path_rank <= 3 THEN 1 ELSE 0 END AS top_3_flag_path, 
+
+CASE       -- find weeks where incidents dropped 
+    When incidents_weekly < avg_incident_per_path THEN 1 ELSE 0 END AS incident_drop,
+
+CASE       -- find week where incidents spiked 
+    WHEN incidents_weekly > avg_incident_per_path THEN 1 ELSE 0 END AS incident_spike, 
+
+CASE       -- flag weeks where hours dropped
+    WHEN fclm_hours <  avg_hours_per_path THEN 1 ELSE 0 END AS hours_drop, 
+
+CASE       -- alert weeks where hours spiked 
+    WHEN fclm_hours > avg_hours_per_path THEN 1 ELSE 0 END AS hours_spike
+ 
 FROM ranked;                                      
-
-
-
 
 
--- sql/features/feat_site_week_hours_risk.sql
+++ sql/features/feat_site_week_hours_risk.sql
@@ -1,4 +1,8 @@
 -- features build out for all incidents x exposure dataset 
+-- Output indicators: 
+--  KPI: YTD metircs 
+--  Signal: Weekly incident rate/hours + weekly normalized rates
+--  Trend: Rolling averages + Rolling standard deviation (volatility over time) 
 
 DROP VIEW IF EXISTS feat_site_week_hours_risk; 
 -- import out view for downstream feature engineering 
@@ -8,11 +12,9 @@ SELECT
 site, 
 fiscal_year, 
 week_sum, 
-air_incidents,  
-rate, 
-hours, 
-total_hours_worked, 
-employee_hours
+air_incidents,  -- KPI
+rate as ytd_rate,  -- cumulative rate (kPI) cumalative incidents * 200,000/ culmaltive hours 
+hours as hours_ytd -- KPI
 
 FROM int_site_week_hours
 ), 
@@ -25,46 +27,32 @@ base_weekly AS (
 -- lag: 1 = go back one row
 -- lag: 0 = if there is no previous row, use 0 instead
 
-       (air_incidents - LAG(air_incidents, 1, 0) OVER 
+       (air_incidents - LAG(air_incidents, 1, 0) OVER   
        (PARTITION by site, fiscal_year 
-       ORDER BY week_sum)) as incidents_weekly,
-       
-       (hours - LAG(hours, 1, 0) OVER                 
+       ORDER BY week_sum)) as incidents_weekly,  -- signal 
+
+       (hours_ytd - LAG(hours_ytd, 1, 0) OVER                 
        (PARTITION by site, fiscal_year              
-       ORDER by week_sum)) as hours_weekly,   
-
-        (total_hours_worked - LAG(total_hours_worked, 1, 0) OVER 
-        (PARTITION by site, fiscal_year
-        ORDER BY week_sum)) as total_hours_worked_weekly,
-
-        (employee_hours - LAG(employee_hours, 1, 0) OVER
-        (PARTITION by site, fiscal_year
-        ORDER by week_sum)) as employee_hours_weekly
+       ORDER by week_sum)) as hours_weekly   -- signal 
 
 FROM base), 
                                                         
 rolling AS (    
     SELECT 
         *, 
-        AVG(incidents_weekly) OVER -- rolling incidents 4wk time frame 
+        AVG(incidents_weekly) OVER -- rolling incidents 4wk time frame (Trend)
         (PARTITION by site
         ORDER BY week_sum 
         ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
             AS incidents_4wk_avg, 
 
-        AVG(rate) OVER -- average 4wk rate 
-        (PARTITION by site
-        ORDER by week_sum
-        ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
-            AS rate_4wk_avg,
-
-        AVG(hours_weekly) OVER -- rolling hours 4wk time frame 
+        AVG(hours_weekly) OVER -- rolling hours 4wk time frame (Trend)
         (PARTITION by site
         ORDER by week_sum
         ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
             AS hours_4wk_avg, 
 
-        SQRT (                             -- standard deviation 4wk rolling 
+        SQRT (                             -- standard deviation 4wk rolling (Trend)
             AVG(incidents_weekly * incidents_weekly) OVER
             (PARTITION by site 
             ORDER by week_sum
@@ -84,30 +72,58 @@ rolling AS (
             ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
             )
         )
-    ) as incidents_4wk_std,
+    ) as incidents_4wk_std, 
 
+        SQRT (
+            AVG(hours_ytd * hours_ytd) OVER 
+            (PARTITION BY site
+            ORDER BY week_sum 
+            ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+            )
+            -
+            (
+            AVG(hours_ytd) OVER 
+            (PARTITION by site
+            ORDER BY week_sum 
+            ROWS BETWEEN 3 PRECEDING AND CURRENT ROW 
+            )
+            * 
+            AVG(hours_ytd) OVER 
+            (PARTITION by site
+            ORDER BY week_sum
+            ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+            )
+        )
+    ) as hours_4wk_std
 
-CASE     -- When weekly hours is greater than 0 Logic: display as hours_worked (Hours that have not been adjusted, removed, corrected)
-    WHEN hours_weekly >= 0 THEN hours_weekly ELSE 0 END as hours_worked, 
-CASE     -- If weekly hours is less than 0 Logic: Hours have been adjusted, removed, corrected for that week. 
-    WHEN hours_weekly < 0 THEN hours_weekly ELSE 0 END as hours_adjusted
-            
 FROM base_weekly
-
-) 
+)
 
 SELECT *, 
--- normalized risk
-    incidents_weekly * 10000.0/nullif(total_hours_worked_weekly, 0)
-        as incident_per_10k_hours,
+-- normalized rates
+    incidents_weekly * 10000.0/nullif(hours_weekly, 0) -- incident rate per week (signal)
+        as incidents_per_10k_hours_rate,
 
-    total_hours_worked_weekly * 1.0/nullif(incidents_weekly, 0)
-     as hours_per_incident, 
+    hours_weekly * 1.0/nullif(incidents_weekly, 0) -- hour per weekly ncident rate (signal)
+     as hours_per_incident_rate,
 
+-- deviation from recent baseline 
+     (hours_weekly - hours_4wk_avg) as hours_deviation, -- trend (This flags weeks where hours are unusual or higher relative to history)
+
+-- percent deviation  
+(hours_weekly - hours_4wk_avg) * 1.0/nullif(hours_4wk_avg, 0) AS hours_deviation_pct, -- Trend (This gives a scaled percentage output of whether the week is above or below the norm in terms of hours 
+
+-- z score 
+(hours_weekly - hours_4wk_avg) * 1.0/nullif(hours_4wk_std, 0) AS hours_zscore_4wk, -- Trend (Calculates how many deviations away from norm)
+
+-- flags
 CASE 
-    WHEN incidents_weekly  > incidents_4wk_avg THEN 1 ELSE 0    --logic: if incidents exceed over incident 4wk average flag spike 
-        END as incident_spike_flag 
-
+    WHEN incidents_weekly  > incidents_4wk_avg THEN 1 ELSE 0  --logic: if incidents exceed over incident 4wk average flag spike (signal)
+        END as incident_spike_flag, 
+CASE 
+    WHEN hours_weekly < hours_4wk_avg THEN 1 ELSE 0 END as hours_drop_flag, -- Logic: if hours is greater than 4 week average... return 1 (signal)
+CASE 
+    WHEN hours_weekly > hours_4wk_avg THEN 1 ELSE 0 END as hours_spike_flag -- Logic: If hours is less than 4 week average return 1 (signal)
 
 FROM rolling; 
 
