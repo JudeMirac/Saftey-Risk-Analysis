d460dbb48e644b26739cf764e0c0fb6ee87b0835
JudeMirac <Judesonmirac@gmail.com>
2026-02-11 01:04:25 -0500

    Remove unwanted assets

--- import sqlite3.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-import pandas as pd
-
-with sqlite3.connect('database/Safety.db') as conn:
-    print(pd.read_sql('select * from stg_event_counts where period_name is not null;', conn))
